f867bbb1822401b0158fca9be51ae5449c1647a4
Gracmit <aleksii97@gmail.com>
2025-02-03 19:05:31 +0200

    Ekan viikon tehtävä tiedostojen lisäys

---
+++ Viikko 1/Tehtävät/T1.js
@@ -0,0 +1,86 @@
+// Täytä seuraavien funktioiden sisällöt vastaamaan tehtävän antoa.
+// Varmista myös että kirjoittamasi koodi vastaa javascriptin syntaksia sekä nimeämiskäytäntöjä
+
+
+// Tee funktio joka palauttaa 2 annetun arvon x ja y summan.
+function sumOfTwo(x, y){
+    let sum;
+    return sum;
+}
+
+
+// Tee funktio joka palauttaa 2 annetun arvon x ja y erotuksen
+function differenceOfTwo(x, y){
+    let difference;
+    return difference;
+}
+
+// Tee funktio joka palauttaa 2 annetun arvon x ja y tulon.
+function productOfTwo(x, y){
+    let product;
+    return product;
+}
+
+// Tee funktio, joka palauttaa kuinka monta sekunttia annettu määrä minuutteja on.
+function convertMinutesToSeconds(minutes){
+
+}
+
+// Tee funktio joka palauttaa onko käyttäjä yli 18v. 
+// Jos käyttäjä on, palautetaan teksti: "Käyttäjä on täysi-ikäinen"
+// Jos käyttäjä ei ole, palautetaan teksti: Käyttäjä ei ole täysi-ikäinen
+// Kysy käyttäjältä hänen ikäänsä hyödyntäen prompt()- komentoa
+// Vinkki: string tyyppisen muuttujan pystyy muuttamaan Numberiksi parseInt() fuhnktiota
+function areYouOldEnough(){
+    
+}
+
+
+// Tee funktio joka laskee annetun arvon x kertoman.
+// Kertoma tarkoittaa jokaisen numeron kertolaskua x:ään asti
+// Jos x = 5, kertoma on 1 * 2 * 3 * 4 * 5
+function factorialOfX(x){
+
+}
+
+//Kirjoita ohjelma, joka kysyy käyttäjältä nimen ja muuntaa sen robottimaiseksi (kaikki kirjaimet isoksi ja välien tilalla on -).
+//Esimerkki:
+//Syöte: "Matti Meikäläinen"
+//Tuloste: "MATTI-MEIKÄLÄINEN"
+function robotTalk(){
+
+}
+
+//Kirjoita ohjelma, joka muuntaa kissan iän ihmisvuosiksi (ensimmäinen vuosi = 15, toinen vuosi = 9, sen jälkeen 4 per vuosi).
+// Esimerkki:
+// Syöte: 4 vuotta
+// Tuloste: "Kissasi on 33 ihmisen vuotta vanha!"
+function catYearsCounter(x){
+
+}
+
+
+
+// Testi keissit
+console.log(sumOfTwo(2, 4));
+console.log(sumOfTwo(2, -2));
+console.log("----------------------------");
+console.log(differenceOfTwo(2, -4));
+console.log("----------------------------");
+console.log(productOfTwo(1, 4));
+console.log("----------------------------");
+console.log(convertMinutesToSeconds(500));
+console.log(convertMinutesToSeconds(-5));
+console.log("----------------------------");
+console.log(areYouOldEnough);
+console.log(areYouOldEnough);
+console.log("----------------------------");
+console.log(factorialOfX(6));
+console.log(factorialOfX(2));
+console.log("----------------------------");
+console.log(robotTalk);
+console.log(robotTalk);
+console.log("----------------------------");
+console.log(catYearsCounter(5));
+console.log(catYearsCounter(1));
+console.log(catYearsCounter(2));
---
+++ Viikko 1/Tehtävät/T2.js
@@ -0,0 +1,56 @@
+// Täytä seuraavien funktioiden sisällöt vastaamaan tehtävän antoa.
+// Varmista myös että kirjoittamasi koodi vastaa javascriptin syntaksia sekä nimeämiskäytäntöjä
+
+// kirjoita funktio, mikä palauttaa onko arvo x maaginen numero
+// Maaginen numero on jaollinen sekä 3:lla että 7:llä
+function isMagicalNumber(x){
+
+}
+
+//Pyydä käyttäjältä kuukauden numero (1-12) ja tulosta, montako päivää siinä on.
+// Esimerkki:
+// Syöte: 2
+// Tuloste: "Helmikuussa on 28 tai 29 päivää."
+function howManydaysInAMonth(){
+
+}
+
+
+// Kirjoita ohjelma joka tulostaa konsoliin x apinaa takaperin hyppimässä sängyllä
+// Tuloste jos x = 10:
+// 10 apinaa hyppii sängyllä
+// 9 apinaa hyppii sängyllä
+// ...
+// 1 apina hyppii sängyllä
+// Ei yhtään apinaa hypi sängyllä
+//
+// Hyödynnä silmukkaa tehtävässä
+function monkeyCounter(x){
+
+}
+
+//Pyydä käyttäjää syöttämään salasana ja tarkista:
+// Onko se vähintään 8 merkkiä
+// Sisältääkö se numeron
+// Sisältääkö se erikoismerkin
+// Esimerkki;
+// Syöte: "Salasana123!"
+// Tuloste: Salasanasi on vahva
+function isStrongPassword(){
+
+}
+
+// Kirjoita funktio mikä palauttaa suurimman numeron listan sisältä.
+function biggestNumber(list){
+
+}
+
+
+// Kirjoita funktio mikä palauttaa string tyyppisen muuttujan pisimmän sanan.
+function findLongestWord(x){
+
+}
+
+
+
+// Tee samanlaiset testi keissit kuin T1 tiedostossa oli, jossa testaat funktioita eri syötteillä.
