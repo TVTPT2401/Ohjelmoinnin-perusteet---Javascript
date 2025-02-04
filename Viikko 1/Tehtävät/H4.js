1fd34b2e0861072040d93d38e8fedf0cd903638e
Aleksi Ilmonen <47421249+Gracmit@users.noreply.github.com>
2025-02-04 09:15:57 +0200

    Update H4.js

---
+++ Viikko 1/Tehtävät/H4.js
@@ -0,0 +1,22 @@
+// Täytä seuraavien funktioiden sisällöt vastaamaan tehtävän antoa.
+// Varmista myös että kirjoittamasi koodi vastaa javascriptin syntaksia sekä nimeämiskäytäntöjä
+
+// Kirjoita funktio mikä laskee listassa olevien muuttujien summan. 
+// Jos listassa on muita kuin numeroita, palauta NaN
+function sumCounter(list){
+
+}
+
+// Kirjoita funktio, mikä poistaa listasta duplikaatit ja palauttaa alkuperäisen listan ilman duplikaatteja
+function removeDuplicates(list){
+    
+}
+
+// Luo tähän objekti, jossa on avaimena valtion nimi ja arvona kyseisen valtion pääkaupunki.
+// Lisää ainakin 10 eri valtiota
+
+// Kirjoita funktio mikä palauttaa parametrina annetun valtion pääkaupungin.
+// Etsi ylempänä luodusta objektista 
+function capitalOfCountry(text){
+
+}
