8d9a3c858217cf8d7a5b3f6c05c0bf13a7ba6275
Aleksi Ilmonen <47421249+Gracmit@users.noreply.github.com>
2025-02-04 08:57:04 +0200

    Update H3.js

---
+++ Viikko 1/Tehtävät/H3.js
@@ -0,0 +1,55 @@
+// Täytä seuraavien funktioiden sisällöt vastaamaan tehtävän antoa.
+// Varmista myös että kirjoittamasi koodi vastaa javascriptin syntaksia sekä nimeämiskäytäntöjä
+
+// Kirjoita funktio mikä tulostaa numerot 1-100 konsoliin yksittäin, mutta jos luku on 3:lla jaollinen tulostuu FIZZ
+// ja jos luku on jaollinen 5:llä, tulostuu BUZZ. Jos luku on jaollinen molemmilla, tulostuu FIZZBUZZ
+// Esimerkki tuloste:
+// 1
+// 2
+// FIZZ
+// 4
+// BUZZ
+// FIZZ
+// 7
+// 8
+// FIZZ
+// BUZZ
+// 11
+// FIZZ
+// 13
+// 14
+// FIZZBUZZ
+// ...
+function fizzBuzz(){
+    
+}
+
+
+// Kirjoita funktio mikä tulostaa 20 seuraavaa karkausvuotta.
+function nextLeapYears(){
+    
+}
+
+// Kirjoita funktio mikä palauttaa x pituisen fibonacci lukujonon 
+function fibonacciSequence(x){
+
+}
+
+// Kirjoita funktio mikä testaa onko parametrina annettu teksti panagrammi.
+// Panagrammi on sana/lause, mikä sisältää kaikki aakkoset vähintään kerran.
+function isPangram(text){
+    
+}
+
+// Kirjoita funktio, mikä testaa onko parametrina annettu teksti palindromi
+// Palindromi on sana joka on sama myös takaperin.
+// Esimerkki palindromi: saippuakauppias
+function isPalindrome(text){
+    
+}
+
+// Kirjoita funktio, mikä palauttaa montako sanaa annetussa lauseessa on.
+// Jos sana on tyhjä, palautetaan 0
+function wordCounter(text){
+
+}
